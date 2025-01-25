27c3a97468eecf81ef8988e73903a0aaea87fc61
Natalia Munoz Delaveau <nataliamunozdelaveau@MacBook-Pro-de-Natalia.local>
2025-01-25 10:22:23 -0300

    enero02025

--- assets/js/interacciones.js
+++ assets/js/interacciones.js
@@ -1,15 +1,72 @@
-const dropdownButton = document.querySelector('.dropdown-button');
-const dropdownContent = document.querySelector('.dropdown-content');
+$(document).ready(function() {
+    console.log( "ready!" );
+});
+
+
+//const dropdownContent = document.querySelector('.dropdown-content');
+//const dropdownButton = document.querySelector('.dropdown-button');
+
+//WOM
+// MOSTRAR 
+$(document).ready(function(){  
+    $('#dropdown-button-wom').click(function(){          
+        $('#mypopover').show();
+    });
+   
+});
+// OCULTAR 
+$(document).ready(function(){  
+    $('#btncerrar-wom').click(function(){          
+        $('#mypopover').hide();
+    });
+   
+});
+
+
+
+
+
+// KASTOR 
+// MOSTRAR 
+$(document).ready(function(){  
+    $('#dropdown-button-kastor').click(function(){          
+        $('#mypopoverr').show();
+    });
+   
+});
+// OCULTAR 
+$(document).ready(function(){  
+    $('#btncerrar-kastor').click(function(){          
+        $('#mypopoverr').hide();
+    });
+   
+});
+
+
+
+
+
 
 // Agrega un evento de clic al botón
-dropdownButton.addEventListener('click', () => {
+//dropdownButton.addEventListener('click', () => {
   // Alterna la clase "show" para mostrar u ocultar el contenido
-  dropdownContent.classList.toggle('show');
-});
+  //dropdownContent.classList.toggle('show');
+//});
 
 // Cierra el dropdown si se hace clic fuera de él
-window.addEventListener('click', (event) => {
-  if (!event.target.matches('.dropdown-button')) {
-    dropdownContent.classList.remove('show');
-  }
-});
+//window.addEventListener('click', (event) => {
+ // if (!event.target.matches('.dropdown-button')) {
+  //  dropdownContent.classList.remove('show');
+  //}
+//});
+
+//window.addEventListener('click', (event) => {
+  //  if (!event.target.matches('#btncerrar')) {
+    //  dropdownContent.idList.remove('hide');
+   // }
+ // });
+//
+
+
+
+
